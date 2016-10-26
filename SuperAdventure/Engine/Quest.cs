017ff36f04079c43f618580356857a2b8d09ab92
Brian Snyder <bsnyder@technology4retailers.com>
2016-10-26 15:44:22 -0500

    End of chapter 10

--- SuperAdventure/Engine/Quest.cs
+++ SuperAdventure/Engine/Quest.cs
@@ -14,6 +14,7 @@ namespace Engine
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
         public Item RewardItem { get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
 
         public Quest(int id, string name, string description,
             int rewardExpreiencePoints, int rewardGold)
@@ -23,6 +24,7 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExpreiencePoints;
             RewardGold = rewardGold;
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
     }
 }
