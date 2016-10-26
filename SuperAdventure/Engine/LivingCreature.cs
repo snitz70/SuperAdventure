5921688472ec506c2e44e1f6cb72ed6a32380734
Brian Snyder <bsnyder@technology4retailers.com>
2016-10-26 12:02:16 -0500

    refactored classes according to chapter 7 of LearnCSharpSimpleRPG guide

--- SuperAdventure/Engine/LivingCreature.cs
+++ SuperAdventure/Engine/LivingCreature.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    class HealingPotion : Item
+    public class LivingCreature
     {
-        public int AmountToHeal { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
     }
 }
